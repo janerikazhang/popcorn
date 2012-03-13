5ecc6fff0f8188d5378bdf179aceb658d82c1449
jessieb3@gmail.com <jessieb3@gmail.com@29c5238f-335f-b764-7a79-5abd70529952>
2012-03-13 09:13:16 +0000

    added simple film and actor tables, populated with 'Star Wars Episode IV - A New Hope' and 'Mark Hamill'

--- popcorn_site/popcorn_app/models.py
+++ popcorn_site/popcorn_app/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class Actor(models.Model):
+	first_name = models.CharField(max_length=100)
+	last_name = models.CharField(max_length=100)
+
+	def __unicode__(self):
+		return self.first_name + ' ' + self.last_name
+
+class Film(models.Model):
+	title = models.CharField(max_length=100)
+	actor_id = models.ForeignKey(Actor)
+	genre = models.CharField(max_length=100)
+	keyword = models.CharField(max_length=100)
+	description = models.CharField(max_length=500)
+
+	def __unicode__(self):
+		return self.title
+	
